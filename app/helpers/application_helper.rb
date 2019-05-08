22b21253c5ea45f1e62d6c2e2a069747c4a2fa20
lburnit <leon.ward93@icloud.com>
2019-05-08 10:34:58 +0100

    updated source tracking feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,11 @@ module ApplicationHelper
     end
   end 
 
-  def source_helper(layout_name)
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to 
+      #{ link_to'sign up', new_user_registration_path} to achieve sustainable weightloss."
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end 
 
